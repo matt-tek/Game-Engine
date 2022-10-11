099ff55db1831358ded976d1abecce5492b24d64
matt <mathieu.muty@epitech.eu>
2022-10-11 17:40:29 +0400

    add window but sprite didn't display

--- Ecs/include/AllComponents.hpp
+++ Ecs/include/AllComponents.hpp
@@ -6,3 +6,52 @@
 */
 
 #pragma once
+#include "Game.hpp"
+
+class Transform {
+    public:
+    Transform() = default;
+    void move(float off_x, float off_y) {
+        _pos.x += off_x;
+        _pos.y += off_y;
+    }
+    sf::Vector2f getPos(void) const
+    {
+        return _pos;
+    }
+
+    float getRotation(void) const
+    {
+        return _rotate;
+    }
+    private:
+    sf::Vector2f _pos = {0.0f, 0.0f};
+    float _rotate = 0.0f;
+};
+
+class Gravity {
+    public:
+    Gravity *getComponent(void)
+    {
+        return this;
+    }
+    sf::Vector2f _pos = {0.0f, 0.0f};
+    float _rotate = 0.0f;
+};
+
+class Sprite {
+    public:
+        Sprite() = default;
+        ~Sprite() = default;
+        void setSprite(const std::string &path) {
+            _texture.loadFromFile(path);
+            _sprite.setTexture(_texture);
+        }
+        sf::Sprite getSprite(void) const { return _sprite; }
+        void draw(sf::RenderWindow *window) {
+            window->draw(_sprite);
+        }
+    private:
+        sf::Texture _texture;
+        sf::Sprite _sprite;
+};
